70759d02716084e72c9da656917434310e4fa591
Ashik-Himel <ashikujjamanhimel@gmail.com>
2022-02-18 14:39:11 +0600

    cross icon added on hamburger toggle

--- script.js
+++ script.js
@@ -1,24 +1,15 @@
 const hamburger = document.querySelector('.hamburger');
+const hamburgerIcon = document.querySelector('.fa-bars');
 const aside = document.querySelector('aside');
 const navLink = document.querySelectorAll('.nav-link');
 
 function asideToggle() {
   aside.classList.toggle('active');
+  hamburgerIcon.classList.toggle('fa-times');
 }
 function asideClose() {
   aside.classList.remove('active');
-}
-function eAsideClose(e) {
-  if(aside.contains(e.target)){
-
-  }
-  if(hamburger.contains(e.target)){
-
-  }
-  else {
-    return asideClose();
-  }
+  hamburgerIcon.classList.remove('fa-times');
 }
 hamburger.addEventListener('click' , asideToggle);
 navLink.forEach(n => n.addEventListener('click', asideClose));
-window.addEventListener('click', eAsideClose);
