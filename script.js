93f47df18243a56c51c10e124fd89d32089d9a79
Ashik-Himel <ashikujjamanhimel@gmail.com>
2022-02-18 11:12:44 +0600

    nav script added

--- script.js
+++ script.js
@@ -5,11 +5,20 @@ const navLink = document.querySelectorAll('.nav-link');
 function asideToggle() {
   aside.classList.toggle('active');
 }
-function asideActive() {
-  aside.classList.add('active');
-}
 function asideClose() {
   aside.classList.remove('active');
 }
+function eAsideClose(e) {
+  if(aside.contains(e.target)){
+
+  }
+  if(hamburger.contains(e.target)){
+
+  }
+  else {
+    return asideClose();
+  }
+}
 hamburger.addEventListener('click' , asideToggle);
-navLink.forEach(n <= n.addEventListener('click', asideClose));
+navLink.forEach(n => n.addEventListener('click', asideClose));
+window.addEventListener('click', eAsideClose);
